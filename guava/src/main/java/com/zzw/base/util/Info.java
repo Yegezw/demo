8c17a3c02330326d033a5310bcb523ff8bd6b6ae
ld <zzw422339@163.com>
2024-10-09 12:24:09 +0800

    基础类型工具类

---
+++ guava/src/main/java/com/zzw/base/util/Info.java
@@ -0,0 +1,16 @@
+package com.zzw.base.util;
+
+public class Info
+{
+    /*
+     * Primitive Type    Guava Utilities
+     * byte              Bytes,  SignedBytes,     UnsignedBytes
+     * short             Shorts
+     * int               Ints,   UnsignedInts,    UnsignedInteger
+     * long              Longs,  UnsignedLongs,   UnsignedLong
+     * float             Floats
+     * double            Doubles
+     * char              Chars
+     * boolean           Booleans
+     */
+}
